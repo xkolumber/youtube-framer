1a3cc923c3ffa0607df98e52b2358e01162a169a
xkolumber <lubosk158@gmail.com>
2023-12-24 20:05:08 +0100

    Navbar element

--- src/components/TopLeftImg.tsx
+++ src/components/TopLeftImg.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
 import React from "react";
 
 const TopLeftImg = () => {
-  return <div>TopLeftImg</div>;
+  return (
+    <div
+      className="absolute left-0 top-0 mix-blend-color-dodge z-10 
+    w-[200px] xl:w-[400px] opacity-50"
+    >
+      <Image
+        src="/top-left-img.png"
+        alt="Background image"
+        width={400}
+        height={400}
+      />
+    </div>
+  );
 };
 
 export default TopLeftImg;
