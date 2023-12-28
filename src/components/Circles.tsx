84df821c45ae95619d7ecef5e38ca98faa8a45ad
xkolumber <lubosk158@gmail.com>
2023-12-28 15:34:41 +0100

    About us Page

---
+++ src/components/Circles.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import React from "react";
+
+const Circles = () => {
+  return (
+    <div className="w-[200px] xl:w-[300px] absolute -right-16 -bottom-2 mix-blend-color-dodge animate-pulse duration z-10">
+      <Image
+        src={"/circles.png"}
+        width={260}
+        height={200}
+        className="w-full h-full"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Circles;
